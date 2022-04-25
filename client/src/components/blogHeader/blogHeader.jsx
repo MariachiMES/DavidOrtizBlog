d4a3c320901edcf6a3ea38970d0c60b95963fd2e
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2022-04-25 12:56:10 -0500

    styling on blog finished

--- client/src/components/blogHeader/blogHeader.jsx
+++ client/src/components/blogHeader/blogHeader.jsx
@@ -4,12 +4,10 @@ import "./blogHeader.css";
 export default function BlogHeader() {
   return (
     <section className="blog__header">
-      <header>
-        <div className="container header__container">
-          <h5>Tech Blog</h5>
-          <h1>Code Vomit</h1>
-        </div>
-      </header>
+      <div className="container header__container">
+        <h5>Tech Blog</h5>
+        <h1>Code Vomit</h1>
+      </div>
     </section>
   );
 }
